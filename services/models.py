dcb58ad14ff83bcd4db7368214aebe6fb385f0ae
Mario Flores <mflores@ubymed.com>
2023-11-22 04:46:18 +0000

    updated files

--- services/models.py
+++ services/models.py
@@ -20,7 +20,10 @@ class ConsultaMedica(models.Model):
     nombre = models.CharField(max_length=45)
     descripcion = models.CharField(max_length=45)
     descripcion_larga = models.TextField()
-    costo = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    url = models.CharField(max_length=45)
+    sort_index = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=False)
     def __str__(self):
         return self.nombre
 
@@ -33,7 +36,7 @@ class PruebaLaboratorio(models.Model):
     descripcion = models.TextField()
     sku = models.CharField(max_length=45)
     preparacion = models.TextField(null=True, blank=True)
-    costo = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     laboratorio = models.ForeignKey(SedeLaboratorioClinico, on_delete=models.CASCADE)
     def __str__(self):
         return self.nombre
